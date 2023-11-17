fece06f7191febb677ad3f5b88d76d3234271e00
Irueta <irueta@gmail.com>
2023-11-17 20:17:43 +0100

    1 like por tutorial

--- src/controllers/votos/votosController.js
+++ src/controllers/votos/votosController.js
@@ -5,12 +5,18 @@ const crearVoto = async (req,res)=>{
     const idUsuario=req.session.user.id;
     const idTutorial=req.body.idTutorial;
     const idAutor=req.body.idAutor;
-    // const hasVotado=await votosModel.findVotado(idUsuario, idTutorial);
-    // if(hasVotado){
-    //     throw new Error("Ya le has dado like a este tutorial"), res.redirect("/tutoriales/search");
-    // }
-    const darLike= await votosModel.votar(idUsuario, idTutorial, idAutor);
-    return res.redirect("/tutoriales/search");
+    try{
+        const hasVotado=await votosModel.findVotado(idUsuario, idTutorial);
+        console.log("HASVOTADO",hasVotado)
+        if(hasVotado!==null){
+        return res.redirect("/tutoriales/misTutos");
+        }
+        const darLike= await votosModel.votar(idUsuario, idTutorial, idAutor);
+        return res.redirect("/tutoriales/search");
+
+    }catch(e){
+        console.log(e)
+    }
 }
 
 export default {
